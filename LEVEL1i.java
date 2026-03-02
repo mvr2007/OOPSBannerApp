ee048370e6ac5c0f457b00cd4e860a0490894ec9
mvr2007 <mvrtarun2007@gmail.com>
2026-03-03 01:16:15 +0530

    Add files via upload

---
+++ LEVEL1i.java
@@ -0,0 +1,13 @@
+public class LEVEL1i {
+    public static void main(String[] args) {
+        // Variables for birth year and current year
+        int birthYear = 2000;
+        int currentYear = 2024;
+        
+        // Calculate age
+        int age = currentYear - birthYear;
+        
+        // Display output
+        System.out.println("Harry's age in " + currentYear + " is " + age);
+    }
+}
---
+++ LEVEL1ii.java
@@ -0,0 +1,14 @@
+public class LEVEL2ii {
+    public static void main(String[] args) {
+      
+        int maths = 94;
+        int physics = 95;
+        int chemistry = 96;
+        int totalSubjects = 3;
+        
+
+        double averageMark = (double) (maths + physics + chemistry) / totalSubjects;
+        
+        System.out.println("Sam’s average mark in PCM is " + averageMark);
+    }
+}
